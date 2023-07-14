274f8091c749cb64ade557bc86e5742fdf3762de
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-07-13 17:08:06 -0700

    tasks mand

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    s = 0
+    l = len(sys.argv) - 1
+    for i in range(l):
+        s+= int(sys.argv[i + 1])
+    print("{:d}".format(s))
