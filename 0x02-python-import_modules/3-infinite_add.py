119dbbdb58f8eea8535a934a42898a8aa70e703d
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-07-13 17:21:51 -0700

    3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    
-    s = 0
-    l = len(sys.argv) - 1
-    for i in range(l):
-        s+= int(sys.argv[i + 1])
-    print("{:d}".format(s))
+
+    somme = 0
+    for i in range(len(sys.argv) - 1):
+        somme += int(sys.argv[i + 1])
+    print("{}".format(somme))
