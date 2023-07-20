1b6a7b1c04da0fb7a6fbef02f394aeaed6658a71
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-07-19 20:01:34 -0700

    last

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,6 +5,6 @@ def weight_average(my_list=[]):
     n = 0
     d = 0
     for i in my_list:
-            n += (i[0] * i[1])
-            d += i[1]
+        n += (i[0] * i[1])
+        d += i[1]
     return (n/ d)
