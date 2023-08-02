ba9a835c9a43e11adc3f75937d7e0c7a5d7ba698
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-08-01 18:38:33 -0700

    103é

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
+"""Defines a class MagicClass"""
 import math
+
 
 class MagicClass:
     """This represents a circle"""
     def __init__(self, radius=0):
         """Initializes the Magic Class"""
-        if not isinstance(radius, (int, float)):
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
-    @property
-    def radius(self):
-        """Getter method for the radius attribute"""
-        return self.__radius
-
     def area(self):
-        """Calculates the area of the circle"""
+        """Calculaes the area of the circle"""
         return (self.__radius ** 2) * math.pi
 
     def circumference(self):
