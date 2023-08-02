c476dc84fd099fda79839e7c31d5a79f6491d2f0
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-08-01 18:19:03 -0700

    verific

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""An empty class Square"""
+
+
+class Square:
+    """Define a square"""
+
+    def __init__(self, size=0):
+        """Instantiation with size
+
+        Args:
+            size (int): The size of the new square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
