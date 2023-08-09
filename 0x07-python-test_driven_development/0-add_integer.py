6cac9b0cb9ea520492a0de018670215ab32f7ccd
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-08-09 16:09:36 -0700

    0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,7 +4,7 @@ add_integer(a, b).
 """
 
 
-def add_integer(a, b = 98):
+def add_integer(a, b=98):
     """Return the addition."""
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
