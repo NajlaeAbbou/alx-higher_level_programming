775bed7697791be6e13aeff630352b824143af01
hadj abbou najlae <hadjabbou.najlae@gmail.com>
2023-08-09 16:23:43 -0700

    2

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -9,16 +9,16 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
     x = 0
-    for l in text:
+    for let in text:
         if x == 0:
-            if l == ' ':
+            if let == ' ':
                 continue
             else:
                 x = 1
         if x == 1:
-            if l == '?' or l == '.' or l == ':':
-                print(l)
+            if let == '?' or let == '.' or let == ':':
+                print(let)
                 print()
                 x = 0
             else:
-                print(l, end="")
+                print(let, end="")
